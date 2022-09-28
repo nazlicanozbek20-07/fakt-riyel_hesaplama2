4c7ebfa14c45407bd905b08dc75f384e5d37b819
user <user@DESKTOP-58NVO0I>
2022-09-28 20:31:59 +0300

    Proje dosyası ekle.

---
+++ faktöriyel_hesaplama2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace faktöriyel_hesaplama2
+{
+    internal class Program
+    {
+       public static int faktoriyel(int sayi)
+        {
+          int faktoriyel = 1;
+
+           for (int i = 1; i <= sayi; i++)
+           {
+                faktoriyel = faktoriyel * i;
+           }
+           return faktoriyel;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Değer dizisini giriniz: ");
+            string value = Console.ReadLine();
+            string[] array = value.Split(',');
+
+            int[] intarray=  new int[array.Length];
+
+            for(int i=0; i<array.Length; i++)
+            {
+                string item = array[i];
+                intarray[i] = Int32.Parse(item);
+            }
+
+            string str = " ";
+
+            for(int i=0; i<intarray.Length;i++)
+            {
+                int sonuc = faktoriyel(intarray[i]);                               
+                str += sonuc;
+                if (i != intarray.Length - 1)
+                    str += ",";
+            }
+            
+          
+            Console.Write("Sonuç: " + str);
+            Console.ReadLine();
+        }
+      
+    }
+
+}
---
+++ faktöriyel_hesaplama2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("faktöriyel_hesaplama2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("faktöriyel_hesaplama2")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("58c6ab39-889f-4d63-a67c-0308a959cd8c")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
